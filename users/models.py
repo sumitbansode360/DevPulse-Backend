1013dd9bd4f9e1f7a14baa3a05a03e56f09788c4
sumitbansode360 <sumitbansode360@gmail.com>
2025-12-31 22:54:16 +0530

    username field fixed for user

--- users/models.py
+++ users/models.py
@@ -6,7 +6,6 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)  
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
-        return self.username
+        return self.email
